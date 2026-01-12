7b69191cc2f74ee4eb2af8c78cafcea3f7fe0ea7
Jacky Southiphonh <southiphonhjacky@gmail.com>
2026-01-12 14:28:25 -0500

    lesson finished

--- src/views/event/Layout.vue
+++ src/views/event/Layout.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { onMounted, ref, defineProps, computed } from "vue";
 import EventService from "@/services/EventService.js";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 
 const props = defineProps(["id"]);
 
@@ -8,26 +11,33 @@ const id = computed(() => props.id);
 
 const event = ref(null);
 onMounted(() => {
-  EventService.getEvent(id.value)
-    .then((response) => {
-      event.value = response.data;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    EventService.getEvent(id.value)
+        .then((response) => {
+            event.value = response.data;
+        })
+        .catch((error) => {
+            if (error.response && error.response.status == 404) {
+                router.push({
+                    name: '404Resource',
+                    params: { resource: 'event' }
+                })
+            } else {
+                router.push({ name: 'NetworkError' })
+            }
+        });
 });
 </script>
 
 <template>
-  <div v-if="event">
-    <h1>{{ event.title }}</h1>
-    <div id="nav">
-      <router-link :to="{ name: 'EventDetails' }">Details</router-link>
-      |
-      <router-link :to="{ name: 'EventRegister' }">Register</router-link>
-      |
-      <router-link :to="{ name: 'EventEdit' }">Edit</router-link>
+    <div v-if="event">
+        <h1>{{ event.title }}</h1>
+        <div id="nav">
+            <router-link :to="{ name: 'EventDetails' }">Details</router-link>
+            |
+            <router-link :to="{ name: 'EventRegister' }">Register</router-link>
+            |
+            <router-link :to="{ name: 'EventEdit' }">Edit</router-link>
+        </div>
+        <router-view :event="event" />
     </div>
-    <router-view :event="event" />
-  </div>
 </template>
